3748ebc5158c58aa3d6f581e13a24bb155e30298
Matheus Ferreira <jeronimomatheus05@gmail.com>
2022-10-13 15:46:22 -0300

    identação melhorada

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-// var swiper = new Swiper(".mySwiper", {
-//     cssMode: true,
-//     loop: true,
-//     navigation: {
-//       nextEl: ".swiper-button-next",
-//       prevEl: ".swiper-button-prev",
-//     },
-//     pagination: {
-//       el: ".swiper-pagination",
-//     },
-//     mousewheel: true,
-//     keyboard: true,
-//   });
-
-  let swiperPortfolio = new Swiper(".mySwiper", {
+let swiperPortfolio = new Swiper(".mySwiper", {
     cssMode: true,
     loop: true,
 
