ac63d97b462add12ae1d1121d97aba3e3beffe34
Matheus Ferreira <jeronimomatheus05@gmail.com>
2022-10-13 15:11:08 -0300

    slide adicionados

---
+++ script.js
@@ -0,0 +1,13 @@
+let swiper = new swiper(".mySwiper", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: "swiper-pagination,"
+    },
+    mousewheel: true,
+    keyboard: true
+});
