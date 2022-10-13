c8c051f84a2920d6411786522412b3863c1b5f9d
Matheus Ferreira <jeronimomatheus05@gmail.com>
2022-10-13 15:41:07 -0300

    slider finalizado

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-let swiper = new swiper(".mySwiper", {
+// var swiper = new Swiper(".mySwiper", {
+//     cssMode: true,
+//     loop: true,
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//     },
+//     mousewheel: true,
+//     keyboard: true,
+//   });
+
+  let swiperPortfolio = new Swiper(".mySwiper", {
     cssMode: true,
     loop: true,
+
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
     pagination: {
-        el: "swiper-pagination,"
+        el: ".swiper-pagination",
+        clickable: true,
     },
-    mousewheel: true,
-    keyboard: true
+
 });
